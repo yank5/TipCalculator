b6401293afefa7c16e9c4b9f648668e282a6c2ec
yank5 <yank5@nycstudents.net>
2022-09-29 18:50:44 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,46 @@
 import java.util.Scanner;
-import java.math.MathContext;
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
-
-        float totCost =1;
+//definitions
+        float totCost =0;
         float cost =0;
+        int people;
+        float percent;
 
+        DecimalFormat formatter = new DecimalFormat("#.##");
+//inputs
         Scanner test = new Scanner(System.in);
         System.out.println("Welcome to the tip calculator!");
         System.out.print("People in group: ");
-        int people = test.nextInt();
-        System.out.print("Tip percent: ");
-        int percent = test.nextInt();
-
+        people = test.nextInt();
+        System.out.print("Tip percentage 1-100: ");
+        percent = test.nextInt();
+//cost addition loop
         while(cost!=-1){
             System.out.print("Enter cost in dollar (write -1 to stop) :");
             cost = test.nextFloat();
-            totCost+=cost;
+            if (cost>=0) { //makes sure -1 isnt added to totCost
+                totCost += cost;
+            }
         }
+//math
+        float tip = (percent/100)*(totCost);
+        String tipF = formatter.format(tip);
+        float ppbt = totCost/people;
+        String ppbtF = formatter.format(ppbt);
+        float tcpp = ((totCost+tip)/people);
+        String tcppF = formatter.format(tcpp);
 
-
-
-
+//output
+        System.out.print("Total bill before tip: "+totCost);
+        System.out.print("\nTip percentage: "+percent);
+        System.out.print("\nTotal tip: "+tipF);
+        System.out.print("\nTotal bill with tip: "+(totCost+tip));
+        System.out.print("\nPer person cost before tip: "+ ppbtF);
+        System.out.print("\nTip per person: "+percent/people);
+        System.out.print("\nTotal cost per person: "+tcppF);
 
 
         test.close();
